77ea0f2ce1e317a1d530648b6ba18ed1c338510e
Jeffry <jef.adityap@gmail.com>
2020-09-21 15:45:15 +0700

    service page

--- src/MainRouter.jsx
+++ src/MainRouter.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Main from './Component/Main';
 import Contact from './Component/Contact';
 import Product from './Component/Product';
+import Service from './Component/Service';
 
 function MainRouter() {
   return (
@@ -16,6 +17,9 @@ function MainRouter() {
         </Route>
         <Route exact path="/product">
           <Product />
+        </Route>
+        <Route exact path="/service">
+          <Service />
         </Route>
       </Switch>
     </Router>
