80da34a4432666d979b425e370bc27db13ac0118
fauzanaldiansyah <fauzanaldiansyah@gmail.com>
2020-09-23 22:13:24 +0700

    feat(Contact Page): Create contact page - unfinsihed styling

--- src/Component/Contact/Contact.jsx
+++ src/Component/Contact/Contact.jsx
@@ -1,7 +1,74 @@
 import React from 'react';
+import imageHeader from '../../Assets/Images/ImageContactHeader.png';
+import contact from '../../Assets/Images/ImageContact.png';
+import Header from '../Header'
+import Footer from '../Footer'
+import Grid from '@material-ui/core/Grid'
+import { FormHelperText, TextField, Button } from '@material-ui/core/'
+import useStyles from './ContactStyle.js';
+
 
 function Contact() {
-  return <p>Contact</p>;
+  const classes = useStyles();
+
+  return (
+    <>
+      <div>
+        <img src={imageHeader} alt="Header" width="1920px" height="601px" />
+        <Header />
+      </div>
+      <div>
+        <h1 className={classes.Contact_Title}>Reach Us</h1>
+        <Grid container>
+          <Grid item xs={6} className={classes.Grid_Image}>
+            <div className={classes.Image_Contact}>
+              <img src={contact}
+                className={classes.Contact_Image}
+                width="518px"
+                height="549px"
+                alt="Contact" />
+            </div>
+          </Grid>
+          <Grid item xs={6}>
+            <div className={classes.Form_Contact}>
+              <FormHelperText
+                className={classes.Form_Label}
+              > Your name or company (required) </FormHelperText>
+              <TextField id="outlined-basic" label="Name/Company" variant="outlined" className={classes.Text_Field} />
+              <FormHelperText className={classes.Form_Label}> Your email (required) </FormHelperText>
+              <TextField
+                id="outlined-textarea"
+                label="Email"
+                multiline
+                variant="outlined"
+                className={classes.Text_Field}
+              />
+              <FormHelperText className={classes.Form_Label}> Your message </FormHelperText>
+              <TextField
+                id="outlined-textarea"
+                label="Message"
+                multiline
+                variant="outlined"
+                className={classes.Text_Field}
+              />
+              <div>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  className={classes.Button_Contact}
+                >
+                  Fly It
+                </Button>
+              </div>
+            </div>
+          </Grid>
+        </Grid>
+      </div>
+      <div className={classes.Footer_Contact}>
+        <Footer />
+      </div>
+    </>
+  );
 }
 
 export default Contact;
---
+++ src/Component/Contact/ContactStyle.js
@@ -0,0 +1,59 @@
+import { makeStyles } from '@material-ui/core/styles';
+import { createMuiTheme } from '@material-ui/core';
+
+const theme = createMuiTheme();
+const useStyles = makeStyles({
+  Contact_Title: {
+    color: "#29AE6D",
+    font: 'Montserrat',
+    fontSize: '60px',
+    textAlign: 'center',
+    fontWeight: 'semibold',
+    // width: "288px"
+  },
+  Grid_Image:{
+    display: "flex",
+    alignContent: "center",
+    alignItems: "center"
+  },
+  Image_Contact: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems:'center',
+    alignContent: "center"
+  },
+  Form_Contact: {
+    height: "98px"
+  },
+  Form_Label: {
+    font: "Montserrat",
+    fontSize: "30px",
+    color: "#707070",
+    opacity: 1,
+    margin: "20px"
+  },
+  Text_Field: {
+    width: "732px",
+    height: "98px",
+    // border: "3px solid #707070",
+    borderRadius: "10px",
+    opacity: 1
+  },
+  Button_Contact: {
+    margin: "40px",
+    width: "181px",
+    height: "65px",
+    background: "transparent linear-gradient(112deg, #29AE6D 0%, #009744 100%) 0% 0% no-repeat padding-box",
+    boxShadow: "5px 15px 30px #3939391A",
+    borderRadius: "50px",
+    opacity: 1,
+    font: "Montserrat",
+    fontSize: "24px"
+  },
+  Footer_Contact:{
+    width:"1909px",
+    marginTop:"150px"
+  },
+});
+
+export default useStyles;
