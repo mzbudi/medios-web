02d771c70185de1bbaed4e1e6d0a56451068158c
unknown <ronibudianto361@gmail.com>
2020-09-30 21:45:13 +0700

    merge(Solve-2): resolve product eslint

--- src/Component/Product/Product.jsx
+++ src/Component/Product/Product.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import Header from '../Header'
-import Footer from '../Footer'
-import HeaderImage from '../../Assets/Images/ImageCMSPAGE.png'
-import Logo from '../../Assets//Images/Logo.png'
-import Paperclip from '../../Assets/Icon/paperclip.svg'
-import Monitor from '../../Assets/Icon/monitor.svg'
-import Archive from '../../Assets/Icon/archive.svg'
-import User from '../../Assets/Icon/user-check.svg'
-import Database from '../../Assets/Icon/database.svg'
-import Link from '../../Assets/Icon/link-2.svg'
-import Clock from '../../Assets/Icon/clock.svg'
-import Lock from '../../Assets/Icon/lock.svg'
-import ImageCMS from '../../Assets/Images/CMS Product.png'
-import { Container, Paper, Grid } from '@material-ui/core/'
+import { Container, Paper, Grid } from '@material-ui/core/';
+import Header from '../Header';
+import Footer from '../Footer';
+import HeaderImage from '../../Assets/Images/ImageCMSPAGE.png';
+import Logo from '../../Assets/Images/Logo.png';
+import Paperclip from '../../Assets/Icon/paperclip.svg';
+import Monitor from '../../Assets/Icon/monitor.svg';
+import Archive from '../../Assets/Icon/archive.svg';
+import User from '../../Assets/Icon/user-check.svg';
+import Database from '../../Assets/Icon/database.svg';
+import Link from '../../Assets/Icon/link-2.svg';
+import Clock from '../../Assets/Icon/clock.svg';
+import Lock from '../../Assets/Icon/lock.svg';
+import ImageCMS from '../../Assets/Images/CMS.png';
 import useStyles from './ProductStyle.js';
 
 function Product() {
@@ -23,27 +23,40 @@ function Product() {
         <Grid item xs={12}>
           <Header />
           <img className={classes.Image_Header} src={HeaderImage} alt="CMS" width="1920px" height="1080px" />
-          <p className={classes.Title_CMS}>  CLOUD MEDICAL SYSTEM (CMS) </p>
-          <p className={classes.Title_CMS2}>  MEDICAL RECORD IN YOUR HAND </p>
+          <p className={classes.Title_CMS}> CLOUD MEDICAL SYSTEM (CMS) </p>
+          <p className={classes.Title_CMS2}> MEDICAL RECORD IN YOUR HAND </p>
         </Grid>
       </Grid>
       <div className="Content">
         <Container style={{ textAlign: 'center' }}>
           <img src={Logo} className={classes.Logo} alt="CMS" />
           <p className={classes.Title_CMS3}> CLOUD MEDICAL SYSTEM (CMS) </p>
-          <p className={classes.Title_CMS4}> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae, esse, facilis repudiandae corporis libero quam dolor cupiditate iure numquam, consequuntur rem cumque laboriosam natus ad quis quibusdam non. Explicabo, quo. </p>
+          <p className={classes.Title_CMS4}>
+            {' '}
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae, esse, facilis repudiandae corporis libero
+            quam dolor cupiditate iure numquam, consequuntur rem cumque laboriosam natus ad quis quibusdam non.
+            Explicabo, quo.{' '}
+          </p>
         </Container>
 
         <div className="Problem">
           <Paper className={classes.Container_Problem}>
             <p className={classes.Title_Problem}>Problem</p>
-            <Grid container direction="row" style={{ verticalAlign: 'center', textAlign: 'center', paddingTop: '50px' }}>
+            <Grid
+              container
+              direction="row"
+              style={{ verticalAlign: 'center', textAlign: 'center', paddingTop: '50px' }}
+            >
               <Grid lg={4} item container direction="column">
                 <Grid item>
                   <img src={Paperclip} className={classes.Icon_Image} alt="Paper" />
                 </Grid>
                 <Grid item>
-                  <p className={classes.Problem_Content}> Paper Based Medical Record make patient hard to organized it and difficult to bring it for each check up </p>
+                  <p className={classes.Problem_Content}>
+                    {' '}
+                    Paper Based Medical Record make patient hard to organized it and difficult to bring it for each
+                    check up{' '}
+                  </p>
                 </Grid>
               </Grid>
               <Grid lg={4} item container direction="column">
@@ -51,7 +64,11 @@ function Product() {
                   <img src={Monitor} className={classes.Icon_Image} alt="Supervision" />
                 </Grid>
                 <Grid item>
-                  <p className={classes.Problem_Content}> Lack of supervision from patient make the medical record often fill incomplete and not correct by medical practitioner </p>
+                  <p className={classes.Problem_Content}>
+                    {' '}
+                    Lack of supervision from patient make the medical record often fill incomplete and not correct by
+                    medical practitioner{' '}
+                  </p>
                 </Grid>
               </Grid>
               <Grid lg={4} item container direction="column">
@@ -59,7 +76,11 @@ function Product() {
                   <img src={Archive} className={classes.Icon_Image} alt="Store" />
                 </Grid>
                 <Grid item>
-                  <p className={classes.Problem_Content}> Stored locally on each hospital make doctor in different hospital have limitation about general condition and track record of patient health</p>
+                  <p className={classes.Problem_Content}>
+                    {' '}
+                    Stored locally on each hospital make doctor in different hospital have limitation about general
+                    condition and track record of patient health
+                  </p>
                 </Grid>
               </Grid>
             </Grid>
@@ -71,7 +92,14 @@ function Product() {
           <Grid container>
             <Grid xs={6}>
               <p className={classes.Solution_Content}>MEDICAL RECORD IN YOUR HAND</p>
-              <p className={classes.Solution_Content2}> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              <p className={classes.Solution_Content2}>
+                {' '}
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                deserunt mollit anim id est laborum.
+              </p>
             </Grid>
             <Grid xs={6}>
               <img src={ImageCMS} className={classes.Image_CMS} alt="CMS" />
@@ -80,13 +108,14 @@ function Product() {
         </div>
         <div className={classes.Benefit}>
           <Paper className={classes.Container_Benefit}>
-            <Grid container direction="row" style={{ verticalAlign: 'center', textAlign: 'center', padding: '5px'}}>
-            <Grid lg={1} item />
+            <Grid container direction="row" style={{ verticalAlign: 'center', textAlign: 'center', padding: '5px' }}>
+              <Grid lg={1} item />
               <Grid lg={2} item container direction="column">
                 <Grid item>
                   <img src={User} className={classes.Icon_Image2} alt="User" />
                   <Grid item>
-                    <p className={classes.Benefit_Content}> USER FRIENDLY  </p> </Grid>
+                    <p className={classes.Benefit_Content}> USER FRIENDLY </p>{' '}
+                  </Grid>
                 </Grid>
               </Grid>
               <Grid lg={2} item container direction="column">
@@ -94,7 +123,7 @@ function Product() {
                   <img src={Database} className={classes.Icon_Image2} alt="Easy" />
                 </Grid>
                 <Grid item>
-                  <p className={classes.Benefit_Content}> EASY TO MANAGE  </p>
+                  <p className={classes.Benefit_Content}> EASY TO MANAGE </p>
                 </Grid>
               </Grid>
               <Grid lg={2} item container direction="column">
@@ -118,11 +147,10 @@ function Product() {
                   <img src={Lock} className={classes.Icon_Image2} alt="Secure" />
                 </Grid>
                 <Grid item>
-                  <p className={classes.Benefit_Content}> SECURE  </p>
+                  <p className={classes.Benefit_Content}> SECURE </p>
                 </Grid>
               </Grid>
-              <Grid lg={1} item container/>
-              
+              <Grid lg={1} item container />
             </Grid>
             {/* <Grid container alignItems="center" alignContent="center" textAlign="center" justify="center">
               <Grid xs={1} />
@@ -151,7 +179,7 @@ function Product() {
           </Paper>
         </div>
       </div>
-      <div style={{ marginTop: "300px" }}>
+      <div style={{ marginTop: '300px' }}>
         <Footer />
       </div>
     </>
--- src/Component/Product/ProductStyle.js
+++ src/Component/Product/ProductStyle.js
@@ -1,58 +1,56 @@
 import { makeStyles } from '@material-ui/core/styles';
-import { createMuiTheme } from '@material-ui/core';
 
-const theme = createMuiTheme();
 const useStyles = makeStyles({
   Image_Header: {
-    backgroundSize: 'cover'
+    backgroundSize: 'cover',
   },
   Title_CMS: {
     position: 'absolute',
     top: '50%',
     left: '25%',
-    font:'normal normal bold 60px/73px Montserrat',
-    fontWeight: 'bold'
+    font: 'normal normal bold 60px/73px Montserrat',
+    fontWeight: 'bold',
   },
   Title_CMS2: {
     position: 'absolute',
     top: '65%',
     left: '30%',
     textAlign: 'center',
-    font:'normal normal 600 45px/55px Montserrat',
-    fontWeight: 'bold'
+    font: 'normal normal 600 45px/55px Montserrat',
+    fontWeight: 'bold',
   },
   Title_CMS3: {
     // position: 'absolute',
-    fontSize: "50px",
+    fontSize: '50px',
     // marginTop: '140px',
     font: 'Montserrat',
-    fontFamily: "Montserrat",
+    fontFamily: 'Montserrat',
     fontWeight: 'semibold',
     textAlign: 'center',
     justifyContent: 'center',
     alignItems: 'center',
-    alignContent: 'center'
+    alignContent: 'center',
   },
   Title_CMS4: {
     // position: 'absolute',
-    fontSize: "30px",
+    fontSize: '30px',
     font: 'Montserrat',
-    fontFamily: "Montserrat",
+    fontFamily: 'Montserrat',
     textAlign: 'center',
     justifyContent: 'center',
     alignItems: 'center',
-    alignContent: 'center'
+    alignContent: 'center',
   },
   Logo: {
-    marginTop: "150px",
-    width: "260px",
-    height: "249px",
-    alignItems: 'center'
+    marginTop: '150px',
+    width: '260px',
+    height: '249px',
+    alignItems: 'center',
   },
   Container_Problem: {
-    backgroundColor: "#F2FFF9",
-    width: "1920px",
-    height: "574px",
+    backgroundColor: '#F2FFF9',
+    width: '1920px',
+    height: '574px',
   },
   Title_Problem: {
     font: 'Montserrat',
@@ -60,65 +58,65 @@ const useStyles = makeStyles({
     fontSize: '50px',
     textAlign: 'center',
     paddingTop: '50px',
-    color: 'Black'
+    color: 'Black',
   },
   Icon_Image: {
-    width: "81px",
-    height: "86px",
+    width: '81px',
+    height: '86px',
   },
   Problem_Content: {
-    color: "#707070",
+    color: '#707070',
     font: 'Montserrat',
     fontFamily: 'Montserrat',
     fontSize: '20px',
     textAlign: 'center',
     marginTop: '50px',
     marginLeft: '100px',
-    marginRight: '100px'
+    marginRight: '100px',
   },
   Solution_Content: {
     font: 'Montserrat',
     fontFamily: 'Montserrat',
-    fontSize:'40px',
-    textAlign:'center'
+    fontSize: '40px',
+    textAlign: 'center',
   },
-  Solution_Content2:{
+  Solution_Content2: {
     font: 'Montserrat',
     fontFamily: 'Montserrat',
-    fontSize:'28px',
-    textAlign:'left',
-    margin: "100px"
+    fontSize: '28px',
+    textAlign: 'left',
+    margin: '100px',
   },
   Image_CMS: {
-    width: "674px",
-    height: "426px",
-    margin:"50px"
+    width: '674px',
+    height: '426px',
+    margin: '50px',
   },
-  Benefit:{
-    marginTop: "50px",
-    marginLeft: "400px",
+  Benefit: {
+    marginTop: '50px',
+    marginLeft: '400px',
     textAlign: 'center',
-    alignContent:'center',
-    alignItems:'center',
+    alignContent: 'center',
+    alignItems: 'center',
   },
   Container_Benefit: {
-    backgroundColor: "#F2FFF9",
-    width: "1102px",
-    height: "251px"
+    backgroundColor: '#F2FFF9',
+    width: '1102px',
+    height: '251px',
   },
   Icon_Image2: {
-    width: "75px",
-    height: "75px",
-    alignItems:'center',
-    alignContent:'center',
-    textAlign:'center',
-    marginTop:'30px'
+    width: '75px',
+    height: '75px',
+    alignItems: 'center',
+    alignContent: 'center',
+    textAlign: 'center',
+    marginTop: '30px',
   },
-  Benefit_Content:{
+  Benefit_Content: {
     font: 'Montserrat',
-    fontFamily:'Montserrat',
+    fontFamily: 'Montserrat',
     color: '#707070',
-    fontSize: "25px",
+    fontSize: '25px',
     textAlign: 'center',
   },
 });
