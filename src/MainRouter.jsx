8b617af01352af175fe968da369745e63b14e5b5
alfarih31 <alfarihfz@gmail.com>
2020-10-29 19:40:12 +0700

    feat: use Header & Footer at router

--- src/MainRouter.jsx
+++ src/MainRouter.jsx
@@ -1,16 +1,15 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Route, Switch, useLocation, useParams } from 'react-router-dom';
-import Main from './Component/Main';
-import Contact from './Component/Contact';
-import Product from './Component/Product';
-import Service from './Component/Service';
-import Web from './Component/Services/Web';
-import Desktop from './Component/Services/Desktop';
-import IOT from './Component/Services/IOT';
-import AI from './Component/Services/AI';
-import Analysis from './Component/Services/Analysis';
-import Project from './Component/Project';
-import About from './Component/About';
+import React, { useEffect, Suspense, lazy } from 'react';
+import { BrowserRouter as Router, Route, Switch, useLocation } from 'react-router-dom';
+import BounceLoader from 'react-spinners/BounceLoader';
+import Header from './Component/Header';
+import Footer from './Component/Footer';
+
+const Main = lazy(() => import('./Component/Main'));
+const Contact = lazy(() => import('./Component/Contact'));
+const Product = lazy(() => import('./Component/Product'));
+const ServiceRouter = lazy(() => import('./Component/Service'));
+const Project = lazy(() => import('./Component/Project'));
+const About = lazy(() => import('./Component/About'));
 
 function ScrollToTop() {
   const { pathname } = useLocation();
@@ -26,57 +25,43 @@ function MainRouter() {
   return (
     <Router>
       <ScrollToTop />
-      <Switch>
-        <Route exact path="/">
-          <Main />
-        </Route>
-        <Route path="/contact">
-          <Contact />
-        </Route>
-        <Route path="/product">
-          <Product />
-        </Route>
-        <Route path="/service/:id">
-          <ServiceRouter />
-        </Route>
-        <Route path="/product">
-          <Product />
-        </Route>
-        <Route path="/service">
-          <Service />
-        </Route>
-        <Route path="/project">
-          <Project />
-        </Route>
-        <Route path="/about">
-          <About />
-        </Route>
-      </Switch>
+      <Header />
+      <Suspense fallback={<BounceLoader />}>
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/product" component={Product} />
+          <Route path="/service" component={ServiceRouter} />
+          <Route path="/project" component={Project} />
+          <Route path="/about" component={About} />
+        </Switch>
+      </Suspense>
+      <Footer />
     </Router>
   );
 }
 
-function ServiceRouter() {
-  const { id } = useParams();
-
-  const idSelector = () => {
-    switch (id) {
-      case 'web':
-        return <Web />;
-      case 'desktop':
-        return <Desktop />;
-      case 'ai':
-        return <AI />;
-      case 'iot':
-        return <IOT />;
-      case 'analysis':
-        return <Analysis />;
-      default:
-        return <Service />;
-    }
-  };
-
-  return idSelector();
-}
+// function ServiceRouter() {
+//   const { id } = useParams();
+//
+//   const idSelector = () => {
+//     switch (id) {
+//       case 'web':
+//         return <Web />;
+//       case 'desktop':
+//         return <Desktop />;
+//       case 'ai':
+//         return <AI />;
+//       case 'iot':
+//         return <IOT />;
+//       case 'analysis':
+//         return <Analysis />;
+//       default:
+//         return <Service />;
+//     }
+//   };
+//
+//   return idSelector();
+// }
 
 export default MainRouter;
