5aabe3b8879bba94e8861631862471097fade1f3
kevin dwi putra <kevin.dwiputra6@gmail.com>
2020-11-07 01:12:23 +0700

    refactor: fix all banner style

--- src/Component/Product/ProductCMS.jsx
+++ src/Component/Product/ProductCMS.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Container, Paper, Grid, Typography } from '@material-ui/core/';
-import Footer from '../Footer';
+import { Grid, Typography, Card, CardMedia } from '@material-ui/core/';
 import Logo from '../../Assets/Images/Logo.png';
 import Paperclip from '../../Assets/Icon/paperclip.svg';
 import Monitor from '../../Assets/Icon/monitor.svg';
@@ -15,116 +14,113 @@ import useStyles from './ProductStyle.js';
 
 function ProductCMS() {
   const classes = useStyles();
+  const paperText = `Paper Based Medical Record make patient hard to organized it and difficult to bring it for each
+  check up`;
+  const monitorText = `Lack of supervision from patient make the medical record often fill incomplete and not correct by
+  medical practitioner`;
+  const archiveText = `Stored locally on each hospital make doctor in different hospital have limitation about general
+  condition and track record of patient health`;
+  const problem = [
+    { image: Paperclip, desc: paperText },
+    { image: Monitor, desc: monitorText },
+    { image: Archive, desc: archiveText },
+  ];
+  const benefit = [
+    { image: User, title: 'USER FRIENDLY' },
+    { image: Database, title: 'EASY TO MANAGE' },
+    { image: Link, title: 'INTEGRATED TO DOCTOR' },
+    { image: Clock, title: 'SAVE TIME' },
+    { image: Lock, title: 'SECURE' },
+  ];
   return (
     <>
-      <div className={classes.Image_Header}>
-        <Typography align="center" className={classes.Main_TextService1}>
-          Cloud Medical System (CMS)
-        </Typography>
-        <Typography align="center" className={classes.Main_TextService2}>
-          MEDICAL RECORD IN YOUR HAND
-        </Typography>
-      </div>
-      <div className="Content">
-        <Container style={{ textAlign: 'center' }}>
-          <img src={Logo} className={classes.Logo} alt="CMS" />
-          <p className={classes.Title_CMS3}> CLOUD MEDICAL SYSTEM (CMS) </p>
-          <p className={classes.Title_CMS4}>
-            {' '}
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae, esse, facilis repudiandae corporis libero
-            quam dolor cupiditate iure numquam, consequuntur rem cumque laboriosam natus ad quis quibusdam non.
-            Explicabo, quo.{' '}
-          </p>
-        </Container>
-
-        <div className="Problem">
-          <Paper className={classes.Container_Problem}>
-            <p className={classes.Title_Problem}>Problem</p>
-            <Grid container direction="row" style={{ verticalAlign: 'center', textAlign: 'center' }}>
-              <Grid lg={4} item container direction="column">
-                <Grid item>
-                  <img src={Paperclip} className={classes.Icon_Image} alt="Paper" />
+      <Grid container alignItems="center" alignContent="center" justify="center" className={classes.MainHeader}>
+        <Grid item xs={12}>
+          <Typography align="center" className={classes.PageTitle}>
+            CLOUD MEDICAL SYSTEM (CMS)
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography align="center" className={classes.PageSubtitle}>
+            MEDICAL RECORD IN YOUR HAND
+          </Typography>
+        </Grid>
+      </Grid>
+      <Grid container alignContent="center" justify="center" className={classes.ProductContainer}>
+        <Grid item xs={12}>
+          <Grid container justify="center">
+            <img src={Logo} className={classes.LogoCMS} alt="CMS" />
+          </Grid>
+          <Typography component="p" align="center" className={classes.CMSDesc}>
+            Lorem Ipsum Dolor Sit, Amet Consectetur Adipisicing Elit. Sed Do Euismod Tempor <br />
+            Incididunt Ut Laborelorem <br />
+            (DESKRIPSI SINGKAT)
+          </Typography>
+        </Grid>
+        <Grid item xs={12} className={classes.ProductProblem}>
+          <Grid container direction="column">
+            <Typography component="p" align="center" className={classes.ContentTitle}>
+              Problem
+            </Typography>
+            <Grid container direction="row" justify="center" alignItems="center" alignContent="center">
+              {problem.map((value, index) => (
+                <Grid key={index} item xs={2} className={classes.ProblemContent}>
+                  <img src={value.image} alt={value.image} className={classes.ProblemImg} />
+                  <Typography component="p" align="center" className={classes.ProblemDesc}>
+                    {value.desc}
+                  </Typography>
                 </Grid>
-                <Grid item>
-                  <p className={classes.Problem_Content}>
-                    {' '}
-                    Paper Based Medical Record make patient hard to organized it and difficult to bring it for each
-                    check up{' '}
-                  </p>
-                </Grid>
-              </Grid>
-              <Grid lg={4} item container direction="column">
-                <Grid item>
-                  <img src={Monitor} className={classes.Icon_Image} alt="Supervision" />
-                </Grid>
-                <Grid item>
-                  <p className={classes.Problem_Content}>
-                    {' '}
-                    Lack of supervision from patient make the medical record often fill incomplete and not correct by
-                    medical practitioner{' '}
-                  </p>
-                </Grid>
-              </Grid>
-              <Grid lg={4} item container direction="column">
-                <Grid item>
-                  <img src={Archive} className={classes.Icon_Image} alt="Store" />
-                </Grid>
-                <Grid item>
-                  <p className={classes.Problem_Content}>
-                    {' '}
-                    Stored locally on each hospital make doctor in different hospital have limitation about general
-                    condition and track record of patient health
-                  </p>
-                </Grid>
-              </Grid>
-            </Grid>
-          </Paper>
-        </div>
-
-        <div className="Solution">
-          <p className={classes.Title_Problem}>Solution</p>
-          <Grid container>
-            <Grid item xs={12} lg={6}>
-              <p className={classes.Solution_Content}>MEDICAL RECORD IN YOUR HAND</p>
-              <p className={classes.Solution_Content2}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-                deserunt mollit anim id est laborum.
-              </p>
-            </Grid>
-            <Grid item xs={12} lg={6}>
-              <img src={ImageCMS} className={classes.Image_CMS} alt="CMS" />
+              ))}
             </Grid>
           </Grid>
-        </div>
-
-        <Paper className={classes.Container_Benefit}>
-          <Grid container justify="space-evenly" alignItems="flex-start">
-            <Grid lg md item style={{ textAlign: 'center' }}>
-              <img src={User} className={classes.Icon_Image2} alt="User" />
-              <p className={classes.Benefit_Content}> USER FRIENDLY </p>
+        </Grid>
+        <Grid item xs={12} className={classes.ProductSolution}>
+          <Grid container direction="column">
+            <Typography component="p" align="center" className={classes.ContentTitle}>
+              Solution
+            </Typography>
+            <Grid
+              container
+              direction="row"
+              justify="space-around"
+              alignItems="flex-start"
+              alignContent="center"
+              className={classes.SolutionContent}
+            >
+              <Grid item xs={5}>
+                <Typography component="p" className={classes.CMSTitle}>
+                  MEDICAL RECORD IN YOU HAND
+                </Typography>
+                <Typography component="p" className={classes.CMSDesc}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget ipsum vulputate, molestie
+                  lorem eget, elementum eros. Morbi ut nisi eget augue porttitor tempor vel non turpis. Nulla ex risus,
+                  fringilla fringilla quam nec, iaculis iaculis quam. Nunc auctor auctor quam ac dapibus. Nam ac diam
+                  mollis, faucibus nulla id, lobortis elit. Etiam interdum nisi felis. Vestibulum ante ipsum primis in
+                  faucibus orci luctus et ultrices posuere cubilia curae; Nulla rhoncus molestie nisi a mollis. Aliquam
+                  elementum sollicitudin blandit.
+                </Typography>
+              </Grid>
+              <Grid item xs={5}>
+                <Card className={classes.CardCMS}>
+                  <CardMedia image={ImageCMS} className={classes.CardImg} title="Cloud Medical System" />
+                </Card>
+              </Grid>
             </Grid>
-            <Grid lg md item style={{ textAlign: 'center' }}>
-              <img src={Database} className={classes.Icon_Image2} alt="Easy" />
-              <p className={classes.Benefit_Content}> EASY TO MANAGE </p>
-            </Grid>
-            <Grid lg md item style={{ textAlign: 'center' }}>
-              <img src={Link} className={classes.Icon_Image2} alt="Integrated" />
-              <p className={classes.Benefit_Content}> INTEGRATED TO DOCTOR </p>
-            </Grid>
-            <Grid lg md item style={{ textAlign: 'center' }}>
-              <img src={Clock} className={classes.Icon_Image2} alt="Time" />
-              <p className={classes.Benefit_Content}> SAVE TIME </p>
-            </Grid>
-            <Grid lg md item style={{ textAlign: 'center' }}>
-              <img src={Lock} className={classes.Icon_Image2} alt="Secure" />
-              <p className={classes.Benefit_Content}> SECURE </p>
-            </Grid>
+            <div className={classes.SolutionBenefit}>
+              <Grid container direction="row" justify="center" alignItems="center" alignContent="center">
+                {benefit.map((value, index) => (
+                  <Grid key={index} item xs={2} className={classes.BenefitContent}>
+                    <img src={value.image} alt={value.image} className={classes.BenefitImg} />
+                    <Typography component="p" align="center" className={classes.BenefitTitle}>
+                      {value.title}
+                    </Typography>
+                  </Grid>
+                ))}
+              </Grid>
+            </div>
           </Grid>
-        </Paper>
-      </div>
+        </Grid>
+      </Grid>
     </>
   );
 }
