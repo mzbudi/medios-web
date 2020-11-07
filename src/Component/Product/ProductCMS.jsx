8edbe45475fbbbc02c3af93dac3f42f4e77fac49
alfarih31 <alfarihfz@gmail.com>
2020-11-07 23:56:08 +0700

    refactor CMS page style

--- src/Component/Product/ProductCMS.jsx
+++ src/Component/Product/ProductCMS.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import classnames from 'classnames';
 import { Grid, Typography, Card, CardMedia } from '@material-ui/core/';
 import Logo from '../../Assets/Images/Logo.png';
 import Paperclip from '../../Assets/Icon/paperclip.svg';
@@ -34,90 +35,93 @@ function ProductCMS() {
   ];
   return (
     <>
-      <Grid container alignItems="center" alignContent="center" justify="center" className={classes.MainHeader}>
-        <Grid item xs={12}>
+      <Grid container spacing={1} alignItems="center" justify="center">
+        <Grid item md={12} className={classnames(classes.CMSHeader, classes.CMSBanner)}>
           <Typography align="center" className={classes.PageTitle}>
             CLOUD MEDICAL SYSTEM (CMS)
           </Typography>
-        </Grid>
-        <Grid item xs={12}>
           <Typography align="center" className={classes.PageSubtitle}>
             MEDICAL RECORD IN YOUR HAND
           </Typography>
         </Grid>
-      </Grid>
-      <Grid container alignContent="center" justify="center" className={classes.ProductContainer}>
-        <Grid item xs={12}>
-          <Grid container justify="center">
-            <img src={Logo} className={classes.LogoCMS} alt="CMS" />
-          </Grid>
+        <Grid item md={12} className={classes.CMSHeader}>
+          <img src={Logo} className={classes.LogoCMS} alt="CMS" />
           <Typography component="p" align="center" className={classes.CMSDesc}>
-            Lorem Ipsum Dolor Sit, Amet Consectetur Adipisicing Elit. Sed Do Euismod Tempor <br />
-            Incididunt Ut Laborelorem <br />
-            (DESKRIPSI SINGKAT)
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget ipsum vulputate, molestie lorem
+            eget, elementum eros. Morbi ut nisi eget augue porttitor tempor vel non turpis.
           </Typography>
         </Grid>
-        <Grid item xs={12} className={classes.ProductProblem}>
-          <Grid container direction="column">
-            <Typography component="p" align="center" className={classes.ContentTitle}>
+        <Grid
+          container
+          item
+          md={12}
+          spacing={2}
+          justify="center"
+          alignItems="center"
+          className={classes.ProblemContainer}
+        >
+          <Grid item md={12}>
+            <Typography component="h2" align="center" className={classes.ContentTitle}>
               Problem
             </Typography>
-            <Grid container direction="row" justify="center" alignItems="center" alignContent="center">
-              {problem.map((value, index) => (
-                <Grid key={index} item xs={2} className={classes.ProblemContent}>
-                  <img src={value.image} alt={value.image} className={classes.ProblemImg} />
-                  <Typography component="p" align="center" className={classes.ProblemDesc}>
-                    {value.desc}
-                  </Typography>
-                </Grid>
-              ))}
-            </Grid>
           </Grid>
+          {problem.map((value, index) => (
+            <Grid key={index} item md={3}>
+              <img src={value.image} alt={value.image} className={classes.ProblemImg} />
+              <Typography component="p" align="center" className={classes.ProblemDesc}>
+                {value.desc}
+              </Typography>
+            </Grid>
+          ))}
         </Grid>
-        <Grid item xs={12} className={classes.ProductSolution}>
-          <Grid container direction="column">
+        <Grid
+          container
+          item
+          md={12}
+          spacing={1}
+          justify="center"
+          alignItems="center"
+          className={classes.SolutionContainer}
+        >
+          <Grid item md={12}>
             <Typography component="p" align="center" className={classes.ContentTitle}>
               Solution
             </Typography>
-            <Grid
-              container
-              direction="row"
-              justify="space-around"
-              alignItems="flex-start"
-              alignContent="center"
-              className={classes.SolutionContent}
-            >
-              <Grid item xs={5}>
-                <Typography component="p" className={classes.CMSTitle}>
-                  MEDICAL RECORD IN YOU HAND
+          </Grid>
+          <Grid item md={5}>
+            <Typography component="h3" className={classes.CMSTitle}>
+              MEDICAL RECORD IN YOU HAND
+            </Typography>
+            <br />
+            <Typography component="p" className={classes.CMSDesc}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget ipsum vulputate, molestie lorem
+              eget, elementum eros. Morbi ut nisi eget augue porttitor tempor vel non turpis. Nulla ex risus, fringilla
+              fringilla quam nec, iaculis iaculis quam. Nunc auctor auctor quam ac dapibus. Nam ac diam mollis, faucibus
+              nulla id, lobortis elit. Etiam interdum nisi felis.
+            </Typography>
+          </Grid>
+          <Grid item md={5}>
+            <Card className={classes.CardCMS} elevation={0}>
+              <CardMedia image={ImageCMS} className={classes.CardImg} title="Cloud Medical System" />
+            </Card>
+          </Grid>
+          <Grid
+            container
+            item
+            md={6}
+            direction="row"
+            justify="space-between"
+            alignItems="center"
+            className={classes.SolutionBenefit}
+          >
+            {benefit.map((value, index) => (
+              <Grid key={index} item lg={2}>
+                <img src={value.image} alt={value.image} className={classes.BenefitImg} />
+                <Typography component="p" align="center" className={classes.BenefitTitle}>
+                  {value.title}
                 </Typography>
-                <Typography component="p" className={classes.CMSDesc}>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget ipsum vulputate, molestie
-                  lorem eget, elementum eros. Morbi ut nisi eget augue porttitor tempor vel non turpis. Nulla ex risus,
-                  fringilla fringilla quam nec, iaculis iaculis quam. Nunc auctor auctor quam ac dapibus. Nam ac diam
-                  mollis, faucibus nulla id, lobortis elit. Etiam interdum nisi felis. Vestibulum ante ipsum primis in
-                  faucibus orci luctus et ultrices posuere cubilia curae; Nulla rhoncus molestie nisi a mollis. Aliquam
-                  elementum sollicitudin blandit.
-                </Typography>
               </Grid>
-              <Grid item xs={5}>
-                <Card className={classes.CardCMS}>
-                  <CardMedia image={ImageCMS} className={classes.CardImg} title="Cloud Medical System" />
-                </Card>
-              </Grid>
-            </Grid>
-            <div className={classes.SolutionBenefit}>
-              <Grid container direction="row" justify="center" alignItems="center" alignContent="center">
-                {benefit.map((value, index) => (
-                  <Grid key={index} item xs={2} className={classes.BenefitContent}>
-                    <img src={value.image} alt={value.image} className={classes.BenefitImg} />
-                    <Typography component="p" align="center" className={classes.BenefitTitle}>
-                      {value.title}
-                    </Typography>
-                  </Grid>
-                ))}
-              </Grid>
-            </div>
+            ))}
           </Grid>
         </Grid>
       </Grid>
--- src/Component/Product/ProductStyle.js
+++ src/Component/Product/ProductStyle.js
@@ -2,12 +2,17 @@ import { makeStyles } from '@material-ui/core/styles';
 import HeaderImage from '../../Assets/Images/ImageCMSPAGE.png';
 
 const useStyles = makeStyles(() => ({
-  MainHeader: {
-    height: '100vh',
+  CMSHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    height: '90vh',
+  },
+  CMSBanner: {
     backgroundImage: `url(${HeaderImage})`,
     backgroundSize: 'cover',
     backgroundRepeat: 'no-repeat',
-    backgroundPositionY: '-120px',
   },
   PageTitle: {
     fontFamily: 'Montserrat',
@@ -22,80 +27,76 @@ const useStyles = makeStyles(() => ({
     fontSize: 30,
     color: '#383838',
   },
-  ProductContainer: {
-    marginTop: '5%',
-    marginBottom: '5%',
-  },
   LogoCMS: {
-    width: '18%',
-    marginBottom: '3%',
+    height: '35vh',
+    width: 'auto',
+    marginBottom: '1rem',
   },
   ContentTitle: {
     fontFamily: 'Montserrat',
-    marginTop: '3%',
-    fontSize: 35,
-    fontWeight: 500,
+    fontSize: '2.5rem',
+    fontWeight: 600,
+    paddingTop: '1.875rem',
   },
-  ProductProblem: {
-    marginTop: '5%',
+  ProblemContainer: {
+    height: 'fit-content',
+    width: '100%',
     backgroundColor: '#F2FFF9',
   },
-  ProblemContent: {
-    margin: '5%',
-    height: '35vh',
-  },
   ProblemImg: {
-    width: '40%',
+    width: '5.0625rem',
     display: 'block',
-    marginLeft: 'auto',
-    marginRight: 'auto',
+    margin: 'auto',
   },
   ProblemDesc: {
-    marginTop: '10%',
     fontFamily: 'Montserrat',
-    fontSize: 13,
+    fontSize: '1rem',
+    paddingBottom: '1.875rem',
   },
-  ProductSolution: {
-    marginTop: '2%',
+  SolutionContainer: {
+    height: '100vh',
   },
   SolutionContent: {
     marginTop: '5%',
   },
   CMSTitle: {
     fontFamily: 'Montserrat',
-    fontWeight: 500,
-    fontSize: 20,
-    marginBottom: '5%',
+    fontWeight: 600,
+    fontSize: '1.5625rem',
+  },
+  BannerCMS: {
+    display: 'flex',
+    height: '100vh',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   CMSDesc: {
     fontFamily: 'Montserrat',
-    fontSize: 15,
+    fontSize: '1.25rem',
+    lineHeight: '1.875rem',
+    width: '80%',
   },
   CardCMS: {
-    width: '500px',
+    width: 'auto',
+    height: '50vh',
     borderRadius: '15px',
     marginLeft: 'auto',
     marginRight: 'auto',
   },
   CardImg: {
-    height: '300px',
+    height: '50vh',
+    width: 'auto',
   },
   SolutionBenefit: {
-    margin: 'auto',
-    marginTop: '10%',
-    width: '50%',
     backgroundColor: '#F2FFF9',
-    borderRadius: '20px',
-  },
-  BenefitContent: {
-    height: '20vh',
-    padding: '2%',
+    borderRadius: '1.25rem',
+    padding: '2% 2% 2%',
   },
   BenefitImg: {
     display: 'block',
-    marginLeft: 'auto',
-    marginRight: 'auto',
-    width: '50px',
+    margin: 'auto',
+    width: '4.375rem',
   },
   BenefitTitle: {
     fontSize: 12,
